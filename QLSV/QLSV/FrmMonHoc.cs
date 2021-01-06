bc462dc3b4c2bba9475ceda92d7926836acbd6a6
Trịnh Hồng <trinhhong0702@gmail.com>
2021-01-07 00:46:05 +0700

    them form mon hoc

---
+++ QLSV/QLSV/FrmMonHoc.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLonN6
+{
+    public partial class FrmMonHoc : Form
+    {
+        public FrmMonHoc()
+        {
+            InitializeComponent();
+        }
+       
+
+       
+       
+
+
+        private void btLoadMH_Click(object sender, EventArgs e)
+        {
+           
+            
+           
+        }
+
+        private void btThemMH_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FrmMonHoc_Load(object sender, EventArgs e)
+        {
+            
+
+            
+        }
+
+        private void btXoaMH_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btsuaMH_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void radioButtonTheoKhoa_CheckedChanged(object sender, EventArgs e)
+        {
+            //cbboxchonkhoa.Enabled = true;
+            //cbboxLoadChuyenNganh.Enabled = true;
+        }
+
+        private void radioButtonTatCa_CheckedChanged(object sender, EventArgs e)
+        {
+            //cbboxchonkhoa.Enabled = false;
+            //cbboxLoadChuyenNganh.Enabled = false;
+        }
+
+        private void radioMonHocTheoKhoa_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioMonHocChung_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioMonHocChung2_CheckedChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void radioMonHocTheoKhoa2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        
+        private void cbboxKhoa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+          
+        }
+
+        private void cbboxKhoa1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void cbboxchonkhoa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+       
+        private void cbboxChuyenNganh1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+          
+        }
+    }
+}
