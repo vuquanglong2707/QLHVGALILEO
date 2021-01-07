547614cfc53c1c15a7050902fde6b60c55226b92
minhthuong1162 <minhthuong1162@gmail.com>
2021-01-07 14:51:09 +0700

    Thêm khoa

---
+++ QLSV/QLSV/Khoa.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLonN6
+{
+    public partial class Khoa : Form
+    {
+        public Khoa()
+        {
+            InitializeComponent();
+        }
+        public Boolean click = false;
+
+        private void btLoadkhoa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btThemkhoa_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void btsuakhoa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btXoakhoa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void Khoa_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtSoTien_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void txtSoTien2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void themchuyennganh(String machuyennganh,String tenchuyennganh,String makhoa)
+        {
+            
+        }
+        private void xoachuyennganh(String machuyennganh)
+        {
+            
+        }
+
+        private void suachuyennganh(String machuyennganh, String tenchuyennganh, String makhoa)
+        {
+           
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnCapNhatCNTheoKhoa_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView2_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            
+        }
+    }
+}
