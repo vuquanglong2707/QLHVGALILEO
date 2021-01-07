6b647ce285e844dea2958b0782e26ff2c9034523
Hoadoan132 <77001183+Hoadoan132@users.noreply.github.com>
2021-01-07 23:17:07 +0700

    thêm form nhập điểm

---
+++ QLSV/QLSV/FrmNhapDiemRL.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLonN6
+{
+    public partial class FrmNhapDiemRL : Form
+    {
+        public FrmNhapDiemRL()
+        {
+            InitializeComponent();
+        }
+
+        public Boolean click = false;
+
+        private void FrmNhapDiemRL_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cbboxChonkhoa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnLoadSV_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtTongDiemRL_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void txtTongDiemRL_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            dataGridView1.ClearSelection();
+        }
+    }
+}
