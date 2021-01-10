ce163a96937d6d5e3b322b3a60d16f12de6065e4
Trịnh Hồng <trinhhong0702@gmail.com>
2021-01-10 22:11:44 +0700

    Them form quan ly diem

---
+++ QLSV/QLSV/FrmQLDiem.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BaiTapLonN6
+{
+    public partial class FrmQLDiem : Form
+    {
+        public FrmQLDiem()
+        {
+            InitializeComponent();
+        }
+        static String trangthai;
+        static String chucnang;
+        static int hocky;
+        static String MMH;
+        static int? MaDiemXoa = null;
+        private static String NamHoc;
+        private static String duongdan = null;
+        private void FrmQLDiem_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void cbboxChonlop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+         
+        }
+
+        private void cbboxChonkhoa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void cbboxNamHoc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButtonHK1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButtonHK2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnLoadDanhSach_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnNhapDiem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnCanCel_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnSuaDiem_Click(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void btnXoaDiem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void radioButtonNHAPDIEM_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+        private void export2Excel(DataGridView g, String duongdan)
+        {
+            
+            
+        }
+
+        private void btnXuatExCel_Click(object sender, EventArgs e)
+        {
+           
+               
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+           
+        }
+    }
+}
