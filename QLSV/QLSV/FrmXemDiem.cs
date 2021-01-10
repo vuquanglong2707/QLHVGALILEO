e4f71e4f7e1dd3554d950908e0c70a73f415a562
Trịnh Hồng <trinhhong0702@gmail.com>
2021-01-10 21:58:04 +0700

    Merge branch 'main' of https://github.com/vuquanglong2707/QLHVGALILEO into main
    # Conflicts:
    #	QLSV/.vs/QLSV/v16/.suo
    #	QLSV/QLSV/QLSV.csproj
    #	QLSV/QLSV/obj/Debug/QLSV.csproj.CoreCompileInputs.cache
    #	QLSV/QLSV/obj/Debug/QLSV.csproj.FileListAbsolute.txt
    #	QLSV/QLSV/obj/Debug/QLSV.csproj.GenerateResource.cache
    #	QLSV/QLSV/obj/Debug/QLSV.csprojAssemblyReference.cache

---
+++ QLSV/QLSV/FrmXemDiem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BaiTapLonN6
+{
+    public partial class FrmXemDiem : Form
+    {
+        private String MaSinhVien;
+        public FrmXemDiem(String MaSV)
+        {
+            InitializeComponent();
+            this.MaSinhVien = MaSV;
+        }
+
+        private void FrmXemDiem_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioTheoNHHK_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView2_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            
+        }
+    }
+}
