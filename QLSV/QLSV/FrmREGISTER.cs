bc68574808820a14ba014124202b242300e11061
Vũ Quang Long <quanglong12g@gmail.com>
2021-01-15 20:37:21 +0700

    trinhhong

---
+++ QLSV/QLSV/FrmREGISTER.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLonN6
+{
+    public partial class FrmREGISTER : Form
+    {
+        private String _name;
+        public FrmREGISTER(String user)
+        {
+            InitializeComponent();
+            _name = user;
+        }
+
+        private void FrmREGISTER_Load(object sender, EventArgs e)
+        {
+            
+        }
+    
+        private void changepassword(String user,String pass)
+        {
+            
+        }
+
+        private void btnDoiMatKhau_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+    }
+}
