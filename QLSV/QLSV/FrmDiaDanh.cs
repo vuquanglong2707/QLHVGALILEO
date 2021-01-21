8efcd5d6594b19a79e0a2e8d3ecd6cc6f2937171
minhthuong1162 <minhthuong1162@gmail.com>
2021-01-21 16:02:36 +0700

    Thêm form địa danh

---
+++ QLSV/QLSV/FrmDiaDanh.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLonN6
+{
+    public partial class FrmDiaDanh : Form
+    {
+        public FrmDiaDanh()
+        {
+            InitializeComponent();
+        }
+
+        static String trangthai = null;
+        private void FrmDiaDanh_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void resettinhtp_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void resetquanhuyen_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void resetphuongxa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btthemtinh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btsuatinh_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btxoatinh_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btthemhuyen_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btsuahuyen_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btxoahuyen_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnthemXa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSuaXa_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnXoaXa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void cbboxTTP3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cbboxHuyen_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+           
+        }
+    }
+}
