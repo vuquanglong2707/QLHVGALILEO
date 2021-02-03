23c9af384904ac5f34323485a72fff5e7876a5bf
minhthuong1162 <minhthuong1162@gmail.com>
2021-02-03 16:45:58 +0700

    Thêm form tạo lớp dkmh

---
+++ QLSV/QLSV/FrmTaoLopDangKyHoc.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLonN6
+{
+    public partial class FrmTaoLopDangKyHoc : Form
+    {
+        static String NHHK;
+        public FrmTaoLopDangKyHoc()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmTaoLopDangKyHoc_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void XoaLop(String Malop)
+        {
+           
+        }
+
+        private void XoaSVDANGKYTRONGLOP(String Malop)
+        {
+            
+        }
+
+        private void cbboxKhoa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtSiSo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void btnLoadTheoKhoa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioThayDoi_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            
+        }
+    }
+}
